d62a53aba50bf4dbcf5f3191af45fd323c285957
Dumbrovskii <dumbrovskyy@gmail.com>
2026-09-07 10:33:28 +0300

    refactor: config urls

--- config/urls.py
+++ config/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     path('register/', core_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('edit_profile/', user_views.edit_profile, name='edit-profile'),
-    path('profile/', user_views.profile, name='profile'),
     path('', core_views.home, name='home'),
+    path('', include('users.urls')),
     path('', include('posts.urls')),
+    path('', include('friends.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
